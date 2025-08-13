b589579c6a3e3fa9d71841f56d4db43012ec7a55
Preeti Umesh Halamani <preetiuhalamani123@gmail.com>
2025-08-13 14:54:17 +0530

    tax calulation of employeee

---
+++ day2/tax2.py
@@ -0,0 +1,8 @@
+import tax1 as t1
+
+standard_deduction = 50000  # ₹50,000 deduction
+
+taxable_income = t1.annual_salary - standard_deduction
+
+print("Standard Deduction:", standard_deduction)
+print("Taxable Income:", taxable_income)
