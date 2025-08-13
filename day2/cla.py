c34d2faa29d018a34660ec8a913e962a7b63683e
Preeti Umesh Halamani <preetiuhalamani123@gmail.com>
2025-08-13 19:45:00 +0530

    program

--- day2/cla.py
+++ day2/cla.py
@@ -1,10 +1,8 @@
-
 import sys
 
-print(sys.argv) # printing the argv list
-print(type(sys.argv)) # printing type of argv
-print(sys.argv[0]) # printing 1st element in the list argv
-print(sys.argv[1]) # printing 2nd element in the list argv
+print(sys.argv) #printing the argv
+print(type(sys.argv))
+print(sys.argv[0])
+print(sys.argv[1])
 print(type(sys.argv[0]))
 print(type(sys.argv[1]))
-print(sys.argv[2]) # printing 3rd element in the list argv
