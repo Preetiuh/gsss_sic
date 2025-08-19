9477a816b7a627be630545a20a68fe6ebc4cacd3
Preeti Umesh Halamani <preetiuhalamani123@gmail.com>
2025-08-19 20:01:37 +0530

    slicing programs

---
+++ day5/slicing1.py
@@ -0,0 +1,6 @@
+
+capitals = ['panjim', 'bhubaneswar', 'guwahati', 'aizwal', 'imphal', 'agartala', 'ganktok', 'kohima', 'itanagar', 'shilong']
+
+print(capitals) # by default entire list
+print(capitals[:]) # by default from beginning to end
+print(capitals[::]) # by default from beginning to end and default increment of 1
