0621e04b4e78195ff6dabd728638af490206add2
Preeti Umesh Halamani <preetiuhalamani123@gmail.com>
2025-08-21 15:23:38 +0530

    day6 programs

---
+++ day6/recurssion.py
@@ -0,0 +1,8 @@
+
+def my_function(i):
+    # my_list = ['gsss', 'vvce', 'nie', 'atme', 'mitt']
+    print('From within the recursive function:', i)
+    my_function(i+1) # recursive call
+    sum = 4 + 9
+
+my_function(1)
